027c970f66bc916882fd88f9b8ee0dacdd16b9fa
Jared Brown <jaobrown@iupui.edu>
2018-07-25 08:12:06 -0400

    Delete Config

--- src/Config/config.js
+++
@@ -1,9 +0,0 @@
-// Initialize Firebase
-export const DB_CONFIG = {
-    apiKey: "AIzaSyBKn76LYhgjsi6c3UGzOfEBAz3YTVk5QE8",
-    authDomain: "notes-app72.firebaseapp.com",
-    databaseURL: "https://notes-app72.firebaseio.com",
-    projectId: "notes-app72",
-    storageBucket: "notes-app72.appspot.com",
-    messagingSenderId: "55418387846"
-};
